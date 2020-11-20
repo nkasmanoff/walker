2fc90ec2d2963597f1d928bd6b6d931fcc0a1d7f
Noah Kasmanoff <nkasmanoff@gmail.com>
2020-11-20 11:10:35 -0500

    first commit

---
+++ run_agent.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:k80:1
+##SBATCH --gres=gpu:1
+#SBATCH --mem=10GB
+#SBATCH --job-name=td3
+#SBATCH --mail-type=END
+#SBATCH --output=slurm_%j.out
+
+python BipedalWalkerTD3.py
